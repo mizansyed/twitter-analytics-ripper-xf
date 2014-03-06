d319fff2b7ccae86583f56906a18a310bfff6336
Mizan Syed <mizansyed@gmail.com>
2014-03-06 18:07:26 +0000

    Remove jquery code from inject.js

--- js/page-action.js
+++ js/page-action.js
@@ -1,3 +1,4 @@
+
 $(function() {
 
 	var btnPullData 		= document.getElementById('btn-pull-data');
@@ -6,10 +7,12 @@ $(function() {
 	var loadingIndicator 	= document.getElementById('loading-indicator');
 	var status 				= document.getElementById('status');
 	var dataContainer 		= document.getElementById('data-container');
-
+    var username            = document.getElementById('username');
 
 	btnPullData.addEventListener("click", function () {
-		alert('test');
+
+        loadingIndicator.style.display= 'block';
+
 		chrome.tabs.query({
             active: true,
             currentWindow: true
@@ -18,21 +21,36 @@ $(function() {
                 action: "getanalyticdata"
             }, function(response) {
                 if (response.action == "returndata") {
+                    getCurrentUser(response.username);
                     tabulate(response.rawhtml);
                 }
             });
         });
+
+        loadingIndicator.style.display = 'none';
 	});
 
 	var getCurrentUser = function(data)
 	{
-		
+        username.value = data;
 	}
 
     var tabulate = function(data) {
 
         dataContainer.innerHTML = '';
-        dataContainer.innerHTML = data;
+        dataContainer.innerHTML = data;  
+
+        var content,
+            content_cleaned,
+            tdate, 
+            faves, 
+            rt, 
+            replies, 
+            clicks,
+            row; 
+
+        table_html = "<thead><tr><th>Content</th><th>Clicks</th><th>Date</th><th>Favourites</th><th>ReTweets</th><th>Replies</th></tr></thead>";
+        
     }
 
-});
+});    
