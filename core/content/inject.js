2d4eff0cb087891904ef680847352370691557ec
Mizan Syed <mizansyed@gmail.com>
2014-03-07 01:25:14 +0000

    add datatable support

--- core/content/inject.js
+++ core/content/inject.js
@@ -2,31 +2,31 @@ $(function() {
     chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
 
         if (request.action == "getanalyticdata") {
-        	var username = null,
-        		username_url,
-        		current_user_node;
+            var username = null,
+                username_url,
+                current_user_node;
 
-        	//console.time('jquery');
+            //console.time('jquery');
             //	var html = $('#analytics_tl-table_container').html();
             //console.timeEnd('jquery');
 
-        	//console.time('native');
-            	var html2 = document.getElementById('analytics_tl-table_container');
-            	if (html2)
-            		html = html2.innerHTML;
+            //console.time('native');
+            var html2 = document.getElementById('analytics_tl-table_container');
+            if (html2)
+                html = html2.innerHTML;
             //console.timeEnd('native');
 
             current_user_node = document.getElementsByClassName("current-user")[0];
-            if (current_user_node){       
-            	username_url = current_user_node.getElementsByTagName('a')[0].getAttribute('href');
-            	if (username_url){
-            		user = username_url.match(/https?:\/\/(www\.)?twitter\.com\/(#!\/)?@?([^\/]*)/)[3]; //convert https://twitter.com/username
-            		if (user) username = user.toLowerCase();
-            	}
+            if (current_user_node) {
+                username_url = current_user_node.getElementsByTagName('a')[0].getAttribute('href');
+                if (username_url) {
+                    user = username_url.match(/https?:\/\/(www\.)?twitter\.com\/(#!\/)?@?([^\/]*)/)[3]; //convert https://twitter.com/username
+                    if (user) username = user.toLowerCase();
+                }
             }
 
             sendResponse({
-            	username: username,
+                username: username,
                 rawhtml: html,
                 action: "returndata"
             });
--- js/page-action.js
+++ js/page-action.js
@@ -1,19 +1,19 @@
-
 $(function() {
 
-	var btnPullData 		= document.getElementById('btn-pull-data');
-	var btnExportCsv 		= document.getElementById('btn-export-csv');
-	var btnSaveBtn 			= document.getElementById('btn-save-data');
-	var loadingIndicator 	= document.getElementById('loading-indicator');
-	var status 				= document.getElementById('status');
-	var dataContainer 		= document.getElementById('data-container');
-    var username            = document.getElementById('username');
+    var btnPullData = document.getElementById('btn-pull-data');
+    var btnExportCsv = document.getElementById('btn-export-csv');
+    var btnSaveBtn = document.getElementById('btn-save-data');
+    var loadingIndicator = document.getElementById('loading-indicator');
+    var status = document.getElementById('status');
+    var dataContainer = document.getElementById('data-container');
+    var dataTable = document.getElementById('data-table');
+    var username = document.getElementById('username');
 
-	btnPullData.addEventListener("click", function () {
+    btnPullData.addEventListener("click", function() {
 
-        loadingIndicator.style.display= 'block';
+        loadingIndicator.style.display = 'block';
 
-		chrome.tabs.query({
+        chrome.tabs.query({
             active: true,
             currentWindow: true
         }, function(tabs) {
@@ -28,29 +28,129 @@ $(function() {
         });
 
         loadingIndicator.style.display = 'none';
-	});
+    });
 
-	var getCurrentUser = function(data)
-	{
+
+    var getCurrentUser = function(data) {
+        //if (username.value)
         username.value = data;
-	}
+    };
+
 
     var tabulate = function(data) {
 
         dataContainer.innerHTML = '';
-        dataContainer.innerHTML = data;  
+        dataContainer.innerHTML = data;
+        dataContainer.style.display = 'none';
 
+
+
+        var tweetsRows = $("#analytics_tl tbody tr");
+        var rowNodes = "";
+
+        rowNodes = "<thead><tr><th>Tweet</th><th>Clicks</th><th>Date</th><th>Favourites</th><th>ReTweets</th><th>Replies</th></tr></thead>";
+
+        tweetsRowsLength = tweetsRows.length;
+        for (i = 0; i < tweetsRowsLength; i++) {
+            rowNodes += processRow(tweetsRows[i]);
+        }
+
+        dataTable.innerHTML = "<table class='table' id='out_tbl'>" + rowNodes + "</table>";
+    };
+
+
+    var processRow = function(row) {
+        var thisRow = row;
         var content,
-            content_cleaned,
-            tdate, 
-            faves, 
-            rt, 
-            replies, 
+            contentCleaned,
+            tdate,
+            faves,
+            rt,
+            replies,
             clicks,
-            row; 
+            rowNode;
 
-        table_html = "<thead><tr><th>Content</th><th>Clicks</th><th>Date</th><th>Favourites</th><th>ReTweets</th><th>Replies</th></tr></thead>";
-        
+
+        var entryContentEl = thisRow.getElementsByClassName("entry-content")[0];
+        if (entryContentEl) content = entryContentEl.innerHTML;
+        contentCleaned = content;
+
+        if (content) {
+            //added this to rip out <span> tag which has number of clicks within content
+            contentCleaned = content.replace(/<span class="status-label analytics_tl-link_count tooltip".*>(.*)<\/span>/g, "");
+        } else {
+            return true;
+        }
+
+        tdate = $(thisRow).find("span .timestamp").html();
+        faves = $(thisRow).find(".analytics_tl-faves span").html();
+        rt = $(thisRow).find(".analytics_tl-retweets span").html();
+        replies = $(thisRow).find(".analytics_tl-replies span").html();
+
+        clicks = $(thisRow).find(".analytics_tl-link_count").html();
+        if (empty(clicks)) clicks = "NA";
+        clicks = clicks.replace(" clicks", "");
+
+        return rowNode = "<tr><td>" + contentCleaned + "</td><td> " + clicks + "</td><td> " + tdate + "</td><td>" + faves + "</td><td>" + rt + "</td><td> " + replies + "</td></tr>";
+
+    };
+
+
+    var updateStatus = function(message) {
+
+    };
+
+
+    var empty = function(data) {
+        if (typeof(data) == 'number' || typeof(data) == 'boolean') {
+            return false;
+        }
+        if (typeof(data) == 'undefined' || data === null) {
+            return true;
+        }
+        if (typeof(data.length) != 'undefined') {
+            return data.length == 0;
+        }
+        var count = 0;
+        for (var i in data) {
+            if (data.hasOwnProperty(i)) {
+                count++;
+            }
+        }
+        return count == 0;
+    };
+
+
+    //taken and adpated from http://www.lucaongaro.eu/blog/2012/11/26/dom-selection-without-jquery/
+    var q = function(selector, ctx) {
+        ctx = ctx || document;
+
+        // Return methods for lazy evaluation of the query
+        return {
+
+            // Return array of all matches
+            all: function() {
+                var list, ary = [];
+                list = ctx.querySelectorAll(selector);
+                for (var i = 0; i < list.length; i++) {
+                    ary[i] = list[i];
+                }
+                return ary;
+            },
+
+            // Return first match
+            first: function() {
+                return ctx.querySelector(selector);
+            },
+
+            // Return last match
+            last: function() {
+                var list = ctx.querySelectorAll(selector);
+                return list.length > 0 ? list[list.length - 1] : null;
+            }
+
+        };
+
     }
 
-});    
+});
